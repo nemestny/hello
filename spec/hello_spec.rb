cdffa976c3ba947201fa5ed03f91c6e7ca0b807f
Aleksandr Noskov <nemestny@gmail.com>
2018-07-29 20:10:03 +0500

    init hello gem

---
+++ spec/hello_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hello do
+  it "has a version number" do
+    expect(Hello::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
